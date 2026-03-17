be5e98c3e36f47c207650c02f0ace9f5480eb625
YEHOVAYIRE Moise <moiseyehovayire@gmail.com>
2026-03-17 08:24:23 +0200

    connect auth routes to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const authRoutes = require('./routes/authRoutes');
 
 dotenv.config();
 connectDB();
@@ -8,6 +9,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use('/api/auth',authRoutes);
 
 app.get("/", (req, res) => {
   res.send("API running...");
