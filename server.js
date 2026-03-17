70f051d1f7bfb346a513ea91f28e9b43dc03cf95
YEHOVAYIRE Moise <moiseyehovayire@gmail.com>
2026-03-17 08:19:17 +0200

    connect database to server

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -8,7 +13,7 @@ app.get("/", (req, res) => {
   res.send("API running...");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
