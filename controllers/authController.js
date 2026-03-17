aaafde57ddb3d12456dec98e1df31d25806372bd
YEHOVAYIRE Moise <moiseyehovayire@gmail.com>
2026-03-17 08:33:21 +0200

    add auth controller imports

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,3 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-
-exports.register = async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await User.create({
-      username,
-      email,
-      password: hashedPassword,
-    });
-
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
