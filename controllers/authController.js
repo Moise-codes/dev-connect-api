72e85628bfe2a3b36f4ff41afb390f5b1812a527
YEHOVAYIRE Moise <moiseyehovayire@gmail.com>
2026-03-17 08:37:47 +0200

    add password hashing

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,6 +19,7 @@ const existingUser = await User.findOne({
 if (existingUser) {
   return res.status(400).json({ message: "User already exists" });
 }
+const hashedPassword = await bcrypt.hash(password, 10);
     
   } catch (error) {
     res.status(500).json({ message: "Server error" });
