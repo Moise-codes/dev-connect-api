4e9bccab9f95f0d744b048c4e09c6d15b8325f1a
YEHOVAYIRE Moise <moiseyehovayire@gmail.com>
2026-03-17 08:30:49 +0200

    add password hashing in register

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,20 @@
-exports.register = (req, res) => {
-  res.send("Register controller");
-};
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
 
-exports.login = (req, res) => {
-  res.send("Login controller");
+exports.register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
