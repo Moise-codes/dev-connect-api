771caf524ef89bae449fbb2d2cb7213b4b19554d
YEHOVAYIRE Moise <moiseyehovayire@gmail.com>
2026-03-17 08:43:37 +0200

    add password comparison

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,6 +50,11 @@ const user = await User.findOne({ email });
 if (!user) {
   return res.status(400).json({ message: "Invalid email or password" });
 }
+const isMatch = await bcrypt.compare(password, user.password);
+
+if (!isMatch) {
+  return res.status(400).json({ message: "Invalid email or password" });
+}
     
   } catch (error) {
     res.status(500).json({ message: "Server error" });
