d721a2a392800ffa493fda5859df7ef82ecdb18b
YEHOVAYIRE Moise <moiseyehovayire@gmail.com>
2026-03-17 08:44:10 +0200

    add login response with token

--- controllers/authController.js
+++ controllers/authController.js
@@ -55,6 +55,15 @@ const isMatch = await bcrypt.compare(password, user.password);
 if (!isMatch) {
   return res.status(400).json({ message: "Invalid email or password" });
 }
+res.status(200).json({
+  message: "Login successful",
+  user: {
+    _id: user._id,
+    username: user.username,
+    email: user.email,
+  },
+  token: generateToken(user._id),
+});
     
   } catch (error) {
     res.status(500).json({ message: "Server error" });
