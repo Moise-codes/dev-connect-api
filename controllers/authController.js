01d40204eadb71593238555cc25b3e1862c1bc07
YEHOVAYIRE Moise <moiseyehovayire@gmail.com>
2026-03-17 08:34:20 +0200

    add jwt token generator

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,8 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+};
