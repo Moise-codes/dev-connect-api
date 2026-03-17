008772e8ae475379a0c4e25da604dc299b98fb6b
YEHOVAYIRE Moise <moiseyehovayire@gmail.com>
2026-03-17 08:38:17 +0200

    create new user in database

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,6 +20,11 @@ if (existingUser) {
   return res.status(400).json({ message: "User already exists" });
 }
 const hashedPassword = await bcrypt.hash(password, 10);
+const user = await User.create({
+  username,
+  email,
+  password: hashedPassword,
+});
     
   } catch (error) {
     res.status(500).json({ message: "Server error" });
